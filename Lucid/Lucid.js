2ce0df80b7629c6079b68b3fb8dc99d0ee6e3ee1
CodeInside <scgtdy7151@gmail.com>
2015-06-21 00:00:35 +0900

    stereo test

--- Lucid/Lucid.js
+++ Lucid/Lucid.js
@@ -42,8 +42,9 @@ if(!(_MAIN_DATA.exists())) {
 }
 
 var Assets = {};
-Assets.warning = java.io.File(_MAIN_DIR, "assets01.res");
-Assets.warning2 = java.io.File(_MAIN_DIR, "assets02.res");
+Assets.warning1 = new java.io.File(_MAIN_DIR, "assets01.res");
+Assets.warning2 = new java.io.File(_MAIN_DIR, "assets02.res");
+Assets.mus1 = new java.io.File(_MAIN_DIR, "assets03.res");
 
 function en_US() {
 	this.multiWarningMsg = "Multiplay is not support";
@@ -65,7 +66,7 @@ var Lucid = {};
 Lucid.isRunning = false;
 Lucid.currentStage = 0;
 Lucid.stageTicking = false;
-Lucid.stageData = [];
+Lucid.stageData = [{name: "idle", init: null, tick: null, fin: null, point: [{x: null, y: null, z: null, f: function() {clientMessage(Player.getX())}, b: function() {print(Player.getZ())}, bn: "Active"}], loc: [], ent: []}];
 //Lucid.stageData.push({name: "test Stage", init: function() {}, tick: function() {}, fin: function() {}, point: [], loc: [], ent: []});
 //Lucid.stageData[Lucid.currentStage].point.push({x: 0, y: 0, z: 0, f: function() {}, b: function() {}, bn: "Active"});
 //Lucid.stageData[Lucid.currentStage].loc.push({sx: 0, ex: 0, sy: 0, ey: 0, sz: 0, ez: 0, f: function() {}, b: function() {}, bn: "Active"});
@@ -86,18 +87,20 @@ var entitySpawnerData = [];
 var setTileManagerData = [];
 //[x, y, z, id, data]
 var setTileSpeed = 1;
-var bgmManagerData = [];
+var bgsData = [];
 var screenColorData = [];
 Lucid.lastColor = 0;
 
 function findStageIdByName(stageName) {try {
 	for(var e = 0; e < Lucid.stageData.length; e++) {
 		if(Lucid.stageData[e].name === stageName) {
+			debug("findStageIdByName", "name: " + stageName + " index: " + e);
 			return e;
 		}
 	}
 	throw new ReferenceError(stageName + " stage is not in the Lucid.stageData");
 }catch(e) {
+	debug("findStageIdByName", "errorLine: " + e.lineNumber);
 	showError(e);
 }}
 
@@ -105,22 +108,28 @@ function multiChecker() {try {
 	var a = Entity.getAll();
 	for(var e = 0; e < a.length; e++) {
 		if(Player.isPlayer(a[e]) && !EntityFix.isEqual(a[e], Player.getEnity())) {
-			warningPopup(lang.multiWarningMsg);
+			debug("multiChecker", "ban: " + Player.getName(a[e]));
+			Entity.setHealth(a[e], 0);
 		}
 	}
 }catch(e) {
+	debug("multiChecker", "errorLine: " + e.lineNumber);
 	showError(e);
 }}
 
 function screenColorLoad() {try {
 	Lucid.screenFilter = new android.widget.RelativeLayout(ctx);
 	var wd = new android.widget.PopupWindow(Lucid.screenFilter, android.widget.RelativeLayout.LayoutParams.MATCH_PARENT, android.widget.RelativeLayout.LayoutParams.MATCH_PARENT, false);
+	wd.setTouchable(false);
 	uiThread(function() {try {
-		wd.showAtLoction(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT|android.view.Gravity.TOP, 0, 0);
+		wd.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT|android.view.Gravity.TOP, 0, 0);
+		debug("screenColorLoad", "loaded");
 	}catch(e) {
+		debug("screenColorLoad-ui", "errorLine: " + e.lineNumber);
 		showError(e);
 	}});
 }catch(e) {
+	debug("screenColorLoad", "errorLine: " + e.lineNumber);
 	showError(e);
 }}
 
@@ -133,12 +142,14 @@ function screenColorManager() {try {
 				Lucid.screenFilter.setBackgroundColor(android.graphics.Color.rgb(c[1], c[2], c[3]));
 				Lucid.screenFilter.setAlpha(c[0]);
 			}catch(e) {
+				debug("screenColorManager-ui", "errorLine: " + e.lineNumber);
 				showError(e);
 			}});
 			Lucid.lastColor = i;
 		}
 	}
 }catch(e) {
+	debug("screenColorManager", "errorLine: " + e.lineNumber);
 	showError(e);
 }}
 
@@ -158,7 +169,7 @@ function warningPopup(str) {try {
 	if(Assets.warning.exists()) {
 		var iv = new android.widget.ImageView(ctx);
 		var iv_b = android.graphics.BitmapFactory.decodeFile(Assets.warning.getAbsolutePath());
-		var iv_b2 = android.graphics.Bitmap.createScaledBitmap(iv_b, PIXEL*300, PIXEL*240, false);
+		var iv_b2 = android.graphics.Bitmap.createScaledBitmap(iv_b, PIXEL*150, PIXEL*120, false);
 		iv.setImageBitmap(iv_b2);
 		ll.addView(iv);
 	}
@@ -171,9 +182,12 @@ function warningPopup(str) {try {
 	ll.addView(tv);
 	
 	var wd = new android.widget.PopupWindow(ll, android.widget.RelativeLayout.LayoutParams.MATCH_PARENT, android.widget.RelativeLayout.LayoutParams.MATCH_PARENT, false);
+	wd.setTouchable(false);
+	debug("warningPopup", "show: " + str);
 	uiThread(function() {try {
 		wd.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT|android.view.Gravity.TOP, 0, 0);
 	}catch(e) {
+		debug("warningPopup-ui1", "errorLine: " + e.lineNumber);
 		showError(e);
 	}});
 	thread(function() {try {
@@ -182,6 +196,7 @@ function warningPopup(str) {try {
 				ll_g.mutate().setAlpha(sf.shift() + 55);
 				ll.setBackgroundDrawable(ll_g);
 			}catch(e) {
+				debug("warningPopup-ui2", "errorLine: " + e.lineNumber);
 				showError(e);
 			}});
 			sleep(50);
@@ -189,18 +204,76 @@ function warningPopup(str) {try {
 		uiThread(function() {try {
 			wd.dismiss();
 		}catch(e) {
+			debug("warningPopup-ui3", "errorLine: " + e.lineNumber);
 			showError(e);
 		}});
 	}catch(e) {
+		debug("warningPopup-thread", "errorLine: " + e.lineNumber);
 		showError(e);
 	}}).start();
 }catch(e) {
+	debug("warningPopup", "errorLine: " + e.lineNumber);
 	showError(e);
 }}
 
-function userPointedBlockData() {
-	
-}
+function userPointedBlockData() {try {
+	var x = Player.getPointedBlockX();
+	var y = Player.getPointedBlockY();
+	var z = Player.getPointedBlockZ();
+	if(Lucid.lastPointX !== x || Lucid.lastPointY !== y || Lucid.lastPointZ !== z) {
+		Lucid.lastPointX = x;
+		Lucid.lastPointY = y;
+		Lucid.lastPointZ = z;
+		activeButtonDismiss();
+		for(var e = 0; e < Lucid.stageData[Lucid.currentStage].point.length; e++) {
+			if(Lucid.stageData[Lucid.currentStage].point[e].x === x && Lucid.stageData[Lucid.currentStage].point[e].z === z && Lucid.stageData[Lucid.currentStage].point[e].y === y) {
+				Lucid.stageData[Lucid.currentStage].point[e].f();
+				activeButton(Lucid.stageData[Lucid.currentStage].point[e].bn, Lucid.stageData[Lucid.currentStage].point[e].b);
+			}
+		}
+	}
+}catch(e) {
+	debug("userPointedBlockData", "errorLine: " + e.lineNumber);
+	showError(e);
+}}
+
+function userPointedEntData() {try {
+	var ent = Player.getPointedEntity();
+	if(!EntityFix.isEqual(Lucid.lastPointEnt, ent)) {
+		Lucid.lastPointEnt = ent;
+		activeButtonDismiss();
+		for(var e = 0; e < Lucid.stageData[Lucid.currentStage].point.length; e++) {
+			if(Lucid.stageData[Lucid.currentStage].ent[e].e === ent) {
+				Lucid.stageData[Lucid.currentStage].ent[e].f();
+				activeButton(Lucid.stageData[Lucid.currentStage].ent[e].bn, Lucid.stageData[Lucid.currentStage].ent[e].b);
+			}
+		}
+	}
+}catch(e) {
+	debug("userPointedEntData", "errorLine: " + e.lineNumber);
+	showError(e);
+}}
+
+function userLocationData() {try {
+	var x = Math.floor(Player.getX());
+	var y = Math.floor(Player.getY());
+	var z = Math.floor(Player.getZ());
+	if(Lucid.lastLocX !== x || Lucid.lastLocY !== y || Lucid.lastLocZ !== z) {
+		Lucid.lastLocX = x;
+		Lucid.lastLocY = y;
+		Lucid.lastLocZ = z;
+		activeButtonDismiss();
+		for(var e = 0; e < Lucid.stageData[Lucid.currentStage].loc.length; e++) {
+			if(Lucid.stageData[Lucid.currentStage].loc[e].sx <= x && Lucid.stageData[Lucid.currentStage].loc[e].ex >= x && Lucid.stageData[Lucid.currentStage].loc[e].sz <= z && Lucid.stageData[Lucid.currentStage].loc[e].ez >= z && Lucid.stageData[Lucid.currentStage].loc[e].sy <= y && Lucid.stageData[Lucid.currentStage].loc[e].ey >= y) {
+				Lucid.stageData[Lucid.currentStage].loc[e].f();
+				activeButton(Lucid.stageData[Lucid.currentStage].loc[e].bn, Lucid.stageData[Lucid.currentStage].loc[e].b);
+			}
+		}
+	}
+}catch(e) {
+	debug("userLocationData", "errorLine: " + e.lineNumber);
+	showError(e);
+}}
 
 function activeButtonLoad() {try {
 	if(Lucid.activeWindow == null) {
@@ -210,8 +283,10 @@ function activeButtonLoad() {try {
 		Lucid.activeButton.setTextSize(DIP*16);
 		
 		Lucid.activeWindow = new android.widget.PopupWindow(Lucid.activeButton, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, false);
+		debug("activeButtonLoad", "loaded");
 	}
 }catch(e) {
+	debug("activeButtonLoad", "errorLine: " + e.lineNumber);
 	showError(e);
 }}
 
@@ -219,8 +294,13 @@ function activeButton(text, func) {
 	activeButtonDismiss();
 	uiThread(function() {try {
 		Lucid.activeButton.setText(text);
+		Lucid.activeButton.setOnClickListener(android.view.View.OnClickListener({
+			onClick: func
+		}));
 		Lucid.activeWindow.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.CENTER|android.view.Gravity.BOTTOM, 0, PIXEL*100);
+		Lucid.activeIsShow = true;
 	}catch(e) {
+		debug("activeButton-ui", "errorLine: " + e.lineNumber);
 		showError(e);
 	}});
 }
@@ -230,9 +310,95 @@ function activeButtonDismiss() {
 		uiThread(function() {try {
 			Lucid.activeWindow.dismiss();
 		}catch(e) {
+			debug("activeButtonDismiss-ui", "errorLine: " + e.lineNumber);
 			showError(e);
 		}})
 		Lucid.activeIsShow = false;
+	}
+}
+
+function bgs(x, y, z, file, range, power, vol, loop, stopLoopFunc) {try {
+	var controler = android.media.MediaPlayer();
+	controler.setDataSource(file.getAbsolutePath());
+	controler.setLooping(loop);
+	controler.setVolume(bgsL(x, y, z, range, power)*vol, bgsR(x, y, z, range, power)*vol);
+	controler.prepare();
+	controler.start();
+	bgsData.push({x: x, y: y, z: z, ct: controler, file: file, session: controler.getAudioSessionId(), vol: vol, range: range, power: power, loop: loop, stopLoop: stopLoopFunc});
+}catch(e) {
+	debug("bgs", "errorLine: " + e.lineNumber);
+	showError(e);
+}}
+
+function bgsManager() {try {
+	for(var e = 0; e < bgsData.length; e++) {
+		if(!bgsData[e].ct.isPlaying()) {
+			bgsData[e].ct.release();
+			bgsData.splice(e, 1);
+			continue;
+		}
+		bgsData[e].ct.setVolume(bgsL(bgsData[e].x, bgsData[e].y, bgsData[e].z, bgsData[e].range, bgsData[e].power)*bgsData[e].vol, bgsR(bgsData[e].x, bgsData[e].y, bgsData[e].z, bgsData[e].range, bgsData[e].power)*bgsData[e].vol);
+	}
+}catch(e) {
+	debug("bgsManager", "errorLine: " + e.lineNumber);
+	showError(e);
+}}
+
+function stereoL(x, y, z, power) {
+	var e = locToYaw(Player.getX() - x, Player.getY() - y, Player.getZ() - z);
+	var t = e - Entity.getYaw(Player.getEntity()) + 180 - 135;
+	if(t > 0) {
+		t %= 360;
+	}else {
+		while(t < 0) {
+			t += 360;
+		}
+	}
+	return 1 - (Math.sin(t*Math.PI/180)/power);
+}
+
+function stereoR(x, y, z, power) {
+	var e = locToYaw(Player.getX() - x, Player.getY() - y, Player.getZ() - z);
+	var t = e - Entity.getYaw(Player.getEntity()) + 180 - 45;
+	if(t > 0) {
+		t %= 360;
+	}else {
+		while(t < 0) {
+			t += 360;
+		}
+	}
+	return 1 - (Math.sin(t*Math.PI/180)/power);
+}
+
+function bgsL(x, y, z, range, power) {
+	var distance = Math.sqrt(Math.pow(Player.getY() - y, 2) + Math.pow(Player.getX() - x, 2) + Math.pow(Player.getZ() - z, 2));
+	if(distance < range) {
+		return stereoL(x, y, z, 3 * (range/distance));
+	}else {
+		if((distance - range) * power > 1) {
+			return 0;
+		}
+		var r = stereoL(x, y, z, 3) - ((distance - range) * power);
+		if(r < 0) {
+			return 0;
+		}
+		return r;
+	}
+}
+
+function bgsR(x, y, z, range, power) {
+	var distance = Math.sqrt(Math.pow(Player.getY() - y, 2) + Math.pow(Player.getX() - x, 2) + Math.pow(Player.getZ() - z, 2));
+	if(distance < range) {
+		return stereoR(x, y, z, 3 * (range/distance));
+	}else {
+		if((distance - range) * power > 1) {
+			return 0;
+		}
+		var r = stereoR(x, y, z, 3) - ((distance - range) * power);
+		if(r < 0) {
+			return 0;
+		}
+		return r;
 	}
 }
 
@@ -259,13 +425,47 @@ function entitySpawner() {
 
 function newLevel(str) {
 	activeButtonLoad();
+	screenColorLoad();
+	Lucid.isRunning = true;
+	Lucid.x = Player.getX();
+	Lucid.y = Player.getY();
+	Lucid.z = Player.getZ();
+}
+
+function leaveGame() {
+	Lucid.isRunning = false;
+}
+
+var lag = [];
+function modTick() {
+	lag = [];
+	lag.push(Date.now());
+	userPointedBlockData();
+	lag.push(Date.now());
+	userPointedEntData();
+	lag.push(Date.now());
+	userLocationData();
+	lag.push(Date.now());
+	bgsManager();
+	lag.push(Date.now());
+	if(debugging) {
+		ModPE.showTipMessage("Pblock:" + (lag[1]-lag[0])/1000 + "\nPent:" + (lag[2]-lag[1])/1000 + "\nloc:" + (lag[3]-lag[2])/1000 + "\nbgs:" + (lag[4]-lag[3])/1000);
+	}
 }
 
 function procCmd(str) {
 	if(str == "b") {
-		warningPopup("비둘기 조심");
+		warningPopup("점멸 경고창 활성화");
 	}else {
-		activeButton(str, null);
+		//activeButton(str, null);
+		bgs(Player.getX(), Player.getY(), Player.getZ(), Assets.mus1, 5, 0.1, 1, false, function() {print("not able")});
+	}
+}
+
+function entityAddedHook(ent) {
+	if(Player.isPlayer(ent) && !EntityFix.isEqual(ent, Player.getEntity())) {
+		debug("entityAddedHook", "warning: " + Player.getName(ent));
+		warningPopup(lang.multiWarningMsg);
 	}
 }
 
@@ -288,6 +488,96 @@ EntityFix.findEnt = function(uniqId) {
 		}
 	}
 }
+
+/**
+ * Location(x, y, z) to Vector(yaw, pitch)
+ *
+ * @since 2015-01-??
+ * @author ToonRaOn
+ */
+
+function locToYaw(x, y, z) {
+	var apil = Math.sqrt(Math.pow(x, 2)+Math.pow(z, 2));
+	var apisinHorizontal = x/apil;
+	var apicosHorizontal = z/apil;
+	var apitanHorizontal = x/z;
+	var apiacosHorizontal = Math.acos(z/apil)*180/Math.PI;
+	var apiatanVertical = Math.atan(y/apil);
+	var alpha = 0;
+	if(apisinHorizontal > 0 && apicosHorizontal > 0 && apitanHorizontal > 0)
+		alpha = 360 - apiacosHorizontal;
+	else if(apisinHorizontal > 0 && apicosHorizontal < 0 && apitanHorizontal < 0) 
+		alpha = 360 - apiacosHorizontal;
+	else if(apisinHorizontal < 0 && apicosHorizontal < 0 && apitanHorizontal > 0) 
+		alpha = apiacosHorizontal;
+	else if(apisinHorizontal < 0 && apicosHorizontal > 0 && apitanHorizontal < 0) 
+		alpha = apiacosHorizontal;
+	else if(apicosHorizontal == 1) alpha = 0;
+	else if(apisinHorizontal == 1) alpha = 90;
+	else if(apicosHorizontal == -1) alpha = 180;
+	else if(apisinHorizontal == -1) alpha = 270;
+	else if(apisinHorizontal == 0 && apicosHorizontal == 1 && apitanHorizontal == 0) null;
+	return alpha;
+};
+
+function locToPitch(x, y, z) {
+	return -1 * Math.atan(y / Math.sqrt(Math.pow(x, 2)+Math.pow(z, 2))) * 180 / Math.PI;
+};
+
+
+
+/**
+ * Entity range
+ *
+ * @since 2015-01-??
+ * @author CodeInside
+ */
+
+function rangeEnt(a, b) {
+	return Math.sqrt(Math.pow(Entity.getX(a) - Entity.getX(b), 2) + Math.pow(Entity.getY(a) - Entity.getY(b), 2) + Math.pow(Entity.getZ(a) - Entity.getZ(b), 2));
+};
+
+
+
+/**
+ * Vector(yaw, pitch) to Location(x, y, z)
+ *
+ * @since 2015-01-??
+ * @author CodeInside
+ */
+
+function vectorToX(y, p) {
+	return (-1 * Math.sin(y / 180 * Math.PI) * Math.cos(p / 180 * Math.PI));
+};
+
+function vectorToY(y, p) {
+	return (Math.sin(-p / 180 * Math.PI));
+};
+
+function vectorToZ(y, p) {
+	return (Math.cos(y / 180 * Math.PI) * Math.cos(p / 180 * Math.PI));
+};
+
+
+
+/**
+ * Absolute range x, y, z
+ *
+ * @since 2015-01-??
+ * @author CodeInside
+ */
+
+function absX(x, y, z) {
+	return x / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+};
+
+function absY(x, y, z) {
+	return y / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+};
+
+function absZ(x, y, z) {
+	return z / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+};
 
 /**
  * Error report
@@ -328,15 +618,15 @@ function showError(e) {
  * @author CodeInside
  */
  
-var debugging = false;
-function debug(str) {
+var debugging = true;
+function debug(title, str) {
 	if(debugging) {
-		if(Level.getWorldName() === null) {
+		if(!Lucid.isRunning) {
 			 ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
-		android.widget.Toast.makeText(ctx, "[Debug]\n" + str, android.widget.Toast.LENGTH_LONG).show();
+		android.widget.Toast.makeText(ctx, TAG + "(" + title + ")" + str, android.widget.Toast.LENGTH_SHORT).show();
 			}}));
 		}else {
-			clientMessage("[debug] " + str);
+			clientMessage(ChatColor.GREEN + TAG + "(" + title + ")" + str);
 		}
 	}
 }
